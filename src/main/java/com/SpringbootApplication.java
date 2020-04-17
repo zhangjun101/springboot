6c933506f46ddd92ce4edadabfd3cf0f2e238623
zhangjun <zhangjun@wideal.cn>
2020-04-17 09:23:47 +0800

    22222

--- src/main/java/com/SpringbootApplication.java
+++ src/main/java/com/SpringbootApplication.java
@@ -2,11 +2,19 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
 @SpringBootConfiguration
-public class SpringbootApplication {
+public class SpringbootApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
