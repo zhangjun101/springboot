c270e9af43497e137cd65a8d596c1c9d317e13ff
zhangjun <zhangjun@wideal.cn>
2020-04-17 09:01:48 +0800

    22222

---
+++ src/main/java/com/controller/AreaFenceController.java
@@ -0,0 +1,25 @@
+package com.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * u_area_fence
+ * 
+ * @author zhou
+ * @date 2020-03-25 11:51:56
+ */
+@RestController
+@RequestMapping("/test")
+public class AreaFenceController {
+
+    @GetMapping("/get")
+    @ResponseBody
+    public Object saveAreaFence(){
+        return "a";
+    }
+}
